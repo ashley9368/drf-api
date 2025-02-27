1a55978c2cd66dcea414417cdae6dcb4f73b0e99
ashley9368 <ashleygoward@icloud.com>
2025-02-27 12:14:43 +0000

    update settings

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import re
 
 if os.path.exists('env.py'):
     import env
@@ -60,7 +61,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'drf-api-walkthrough-1.herokuapp.com']
+ALLOWED_HOSTS = [
+    'localhost',
+    os.getenv('ALLOWED_HOST', 'default-value-if-not-set'),  # fallback value if not set
+]
 
 
 # Application definition
@@ -107,6 +111,10 @@ MIDDLEWARE = [
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
+    ]
+elif 'CLIENT_ORIGIN_DEV' in os.environ:
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        r"^https:\/\/.*\.codeinstitute-ide\.net$",
     ]
 else:
     CORS_ALLOWED_ORIGIN_REGEXES = [
