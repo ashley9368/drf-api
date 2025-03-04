75fa96993e34b384c5e8bfc349e810a5c9e73c21
ashley9368 <ashleygoward@icloud.com>
2025-03-04 12:49:50 +0000

    add allowed hosts

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -59,7 +59,12 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'drf-api-walkthrough-1.herokuapp.com']
+ALLOWED_HOSTS = [
+    'localhost', 
+    '127.0.0.1', 
+    'drf-api-walkthrough-1.herokuapp.com',
+    'moments-walkthrough-25-a73b7ad954bc.herokuapp.com',
+]
 
 
 # Application definition
