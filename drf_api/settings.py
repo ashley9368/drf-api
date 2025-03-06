f61c5c8ec7d6c3c07b4f76f94b46266613cfceae
ashley9368 <ashleygoward@icloud.com>
2025-03-06 11:04:24 +0000

    remove line from middleware

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -105,7 +105,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [
